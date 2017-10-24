ad11f0706fc61aee46cce7dd8964a8581945959b
Andrique Liu <andriqueliu@gmail.com>
2017-10-24 13:29:50 -0400

    Get started on fastsort

---
+++ p1/p1a/fastsort.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define EXIT_SUCCESS 0
+
+int main(int argc, char *argv[])
+{
+
+
+        return EXIT_SUCCESS;
+}
+
+void arg_validator(int argc, char *argv[])
+{
+        int more_than_two_args = (argc > 2);
+        int invalid_optional_arg = (argc == 2 && );
+        if (argc > 2 ||) {
+
+        }
+}
