876713bea367872ac47f1a7c52d0de72dbc9184d
Andrique <andriqueliu@gmail.com>
2017-10-24 20:30:47 -0700

    Test some code, refresh a little

--- p1/p1a/fastsort.c
+++ p1/p1a/fastsort.c
@@ -1,35 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define EXIT_SUCCESS 0
 
 void arg_validator(int argc, char *argv[])
 {
-        int more_than_two_args = (argc > 2);
-        int invalid_optional_arg = (
-		argc == 2 &&
-		argv[0][0] == '-');
+        num_args_validator(argc);
+        word_selection_validator(argc, argv);
+}
+
+void num_args_validator(int argc) {
         if (argc > 3) {
-        	exit(1);
+                exit(1);
         }
+}
+
+void word_selection_validator(int argc, char *argv[]) {
         if (argc == 3) {
-        	char dash_check = argv[2][0];
-        	if (dash_check != '-') {
-        		exit(1);
-        	}
-        	char *
+                char *word_selection = argv[2];
+                if (word_selection[0] != '-') {
+                        exit(1);
+                }
+                
+                char *end;
+                long word_selection_value;
+                word_selection_value = strtol((word_selection + 1), &end, 10);
+
+                printf("%ld\n", word_selection_value);
+                printf("%s\n", end);
+
+
         }
 }
 
 int main(int argc, char *argv[])
 {
-	printf("There are %d arguments\n", argc);
-	printf("First argument: %s\n", argv[0]);
-	printf("Second argument: %s\n", argv[1]);
+        printf("There are %d arguments\n", argc);
+        printf("First argument: %s\n", argv[0]);
+        printf("Second argument: %s\n", argv[1]);
 
-	if (!arg_validator(argc, argv)) {
-		printf("BAD\n");
-		return 1;
-	}
+
+        arg_validator(argc, argv);
+
+        printf("Test number: %ld\n", strtol("10", NULL, 10));
+        char *end;
+        printf("Test number: %ld\n", strtol("ff", &end, 10));
+        printf("Crap letter: %s\n", end);
+
+        // // this is UB!
+        // char *plox;
+        // *plox = 'h';
+        // *(plox + 1) = 'e';
+        // printf("%s\n", plox);
+
+        // FILE *file_ptr = 
+
+
 
         return EXIT_SUCCESS;
 }
