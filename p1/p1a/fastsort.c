ef15be2c6bb23986f8d0cfe7166c88017a91bbc4
Andrique <andriqueliu@gmail.com>
2017-10-30 20:57:09 -0700

    Test code

--- p1/p1a/fastsort.c
+++ p1/p1a/fastsort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 10
+#define MAX_LINE_LENGTH 50
 
 #define EXIT_SUCCESS 0
 
@@ -21,10 +21,13 @@ void num_args_validator(int argc);
 void word_selection_validator(int argc, char *argv[]);
 
 
-char **store_lines(char *file_name);
+char **store_lines(char *file_name, int *number_of_lines);
 
 void check_line_length(char *line);
 
+
+
+void free_lines(char **lines, int number_of_lines);
 
 
 int main(int argc, char *argv[])
@@ -50,8 +53,21 @@ int main(int argc, char *argv[])
         // *(plox + 1) = 'e';
         // printf("%s\n", plox);
 
-        char **lines = store_lines(argv[1]);
+        int number_of_lines = 1;
+
+        char *file_name = argv[1];
+
+        char **lines = store_lines(file_name, &number_of_lines);
         
+        // test and print everything out
+
+        printf("Here are some returned lines!\n");
+        int i = 0;
+        while (i < number_of_lines - 1) {
+                printf("%s", lines[i]);
+                i++;
+        }
+
         // from that function, return the array of structs... malloc inside? or allocate from outside
 
 
@@ -65,6 +81,8 @@ int main(int argc, char *argv[])
 
         // then, iterate across all of the structs and print out the sentence
 
+
+        // free_lines(lines, number_of_lines);
 
         return EXIT_SUCCESS;
 }
@@ -101,8 +119,10 @@ void word_selection_validator(int argc, char *argv[])
         }
 }
 
-char **store_lines(char *file_name)
+char **store_lines(char *file_name, int *number_of_lines)
 {
+        // printf("Testing number of lines: %d\n", *number_of_lines);
+
         FILE *fp = NULL;
         fp = fopen(file_name, "r");
         if (fp == NULL) {
@@ -115,22 +135,25 @@ char **store_lines(char *file_name)
 
         char **lines = malloc(sizeof(char *));
         if (lines == NULL) {
-                // some exit stuff
                 fprintf(stderr, "Memory error encountered\n");
+                exit(1);
         }
 
-        int i = 0;
+        int line_i = 0;
+        // size_t curr_size = 1;
         // !!! You don't need to create that struct...
         // just use an array of each line...
         // when it comes time to compare, just deref. down to the appropriate word in the line
 
         // example... so strnlen 
         char line[MAX_LINE_LENGTH];
-        while (fgets(line, MAX_LINE_LENGTH + 1, fp)) {
+        while (fgets(line, MAX_LINE_LENGTH, fp)) {
 
-                printf("%s", line);
+
+                printf("The current line says... %s", line);
                 printf("The length of the current line is... %zd\n", strnlen(line, MAX_LINE_LENGTH));
                 
+                // int line_length = check_line_length(line);
                 check_line_length(line);
 
                 // note: if line is just whitespace, use an empty string!
@@ -138,24 +161,57 @@ char **store_lines(char *file_name)
                 // allocate space for the line!
                 // malloc space for the max line, then copy everything from the line into it!
                 // get the appropriate word
-                *(lines + i) = malloc(MAX_LINE_LENGTH + 1); // maybe + 1?
-                strncpy(*(lines + i), line, MAX_LINE_LENGTH + 1);
+                // *(lines + i) = malloc(MAX_LINE_LENGTH + 1); // maybe + 1?
+                // strncpy(*(lines + i), line, MAX_LINE_LENGTH + 1);
+                lines[line_i] = malloc(MAX_LINE_LENGTH * sizeof(char)); // maybe + 1?
+
+                lines[line_i] = "hello\0";
+                printf("yo !!%s\n", lines[line_i]);
+
+
+                // strncpy(lines[line_i], line, MAX_LINE_LENGTH);
+                char *temp_line = malloc(sizeof(char) * MAX_LINE_LENGTH);
+                strncpy(temp_line, line, MAX_LINE_LENGTH);
+                strncpy(lines[line_i], temp_line, MAX_LINE_LENGTH);
+
+                printf("Just making sure the line was copied: %s\n", lines[line_i]);
+
+
 
                 // realloc
-                i++;
+                line_i++;
+                *number_of_lines += 1;
+
+                printf("Testing... %d\n", *number_of_lines);
+                char **temp_lines = realloc(lines, *number_of_lines);
+                if (temp_lines != NULL) {
+                        lines = temp_lines;
+                } else {
+                        fprintf(stderr, "Memory error encountered\n");
+                        exit(1);
+                }
+                printf("I just realloc\n");
+
         }
         // the end of the array should be null termed
 
+        // lines[line_i] = '\0';
+        printf("I just not nulled\n");
+
+        printf("Gonna close\n");
         fclose(fp);
+        printf("Closed\n");
 
         return lines;
 }
 
 void check_line_length(char *line)
 {
+        int line_length = strnlen(line, MAX_LINE_LENGTH);
+
         if (
-                (strnlen(line, MAX_LINE_LENGTH) == MAX_LINE_LENGTH) &&
-                (line[MAX_LINE_LENGTH - 1] != '\n')) {
+                (line_length == (MAX_LINE_LENGTH - 1)) &&
+                (line[MAX_LINE_LENGTH - 2] != '\n')) {
 
                 fprintf(stderr, "Input line length exceeded limit\n");
                 exit(1);
@@ -164,6 +220,17 @@ void check_line_length(char *line)
 
 
 
+// iterate through the array, freeing each element as you go
+// finally, free the char array
+void free_lines(char **lines, int number_of_lines)
+{
+        int i = 0;
 
+        while (i < number_of_lines - 1) {
+                free(lines[i]);
+                i++;
+        }
 
+        free(lines);
+}
 
