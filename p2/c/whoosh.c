756793359698b80ccc1694427e4ec7bd0788bd6b
Andrique <andriqueliu@gmail.com>
2017-11-24 19:41:58 -0800

    Make some progress!

--- p2/c/whoosh.c
+++ p2/c/whoosh.c
@@ -2,9 +2,68 @@
  * 
  */
 
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+#define MAX_LINE_LENGTH 2
+
+
+#define EXIT_CMD "exit"
+
+#define EXIT_SUCCESS 0
+
+
+char error_message[30] = "An error has occurred\n";
+
+
+// Function Prototypes
+
+// parse_cmd();
+
+void check_line_length(char *line);
+
+
+void whoosh_exit();
+
 int main(int argc, char *argv[])
 {
+	// while (1) {
+	// 	printf("whoosh> ");
+	// 	parse_cmd();
+	// }
 
+	check_line_length(argv[1]);
+
+	return EXIT_SUCCESS;
+}
+
+// 
+// void parse_cmd()
+// {
+// 	// use fgets
+// 	// make sure line isn't too long though
+
+
+
+// }
+
+void check_line_length(char *line)
+{
+	if (strlen(line) > MAX_LINE_LENGTH) {
+		// print error and exit
+	}
+}
+
+
+// func to print error message and exit
+void whoosh_exit()
+{
+	write(STDERR_FILENO, error_message, strlen(error_message));
+	// execv... call the system exit() method
 }
 
 
