2aab0dbc4da21a79d5d0a5734f86af6afd439749
Andrique <andriqueliu@gmail.com>
2017-11-26 10:49:57 -0800

    check built ins

--- p2/c/whoosh.c
+++ p2/c/whoosh.c
@@ -4,6 +4,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
@@ -12,7 +13,14 @@
 #define MAX_LINE_LENGTH 2
 
 
-#define EXIT_CMD "exit"
+#define BUILT_INS 4
+
+
+#define TRUE 1
+#define FALSE 0
+
+
+#define EXIT_command "exit"
 
 #define EXIT_SUCCESS 0
 
@@ -22,10 +30,11 @@ char error_message[30] = "An error has occurred\n";
 
 // Function Prototypes
 
-// parse_cmd();
+// parse_command();
 
 void check_line_length(char *line);
 
+int check_built_in(char *command);
 
 void whoosh_exit();
 
@@ -33,16 +42,21 @@ int main(int argc, char *argv[])
 {
 	// while (1) {
 	// 	printf("whoosh> ");
-	// 	parse_cmd();
+	// 	parse_command();
 	// }
 
 	check_line_length(argv[1]);
+
+
+	
+
+	whoosh_exit();
 
 	return EXIT_SUCCESS;
 }
 
 // 
-// void parse_cmd()
+// void parse_command()
 // {
 // 	// use fgets
 // 	// make sure line isn't too long though
@@ -58,12 +72,47 @@ void check_line_length(char *line)
 	}
 }
 
+// check if built in
+int check_built_in(char *command)
+{
+	char *built_ins[] = {
+		"exit",
+		"cd",
+		"pwd",
+		"path"
+	};
 
-// func to print error message and exit
+	for (int i = 0; i < BUILT_INS; i++) {
+		if (strcmp(command, built_ins[i]) == 0) {
+			return TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
+// ??? for the IMPLEMENTED functions, should
+// wait, execv, etc. be used?
+// Probably not...
+
 void whoosh_exit()
 {
-	write(STDERR_FILENO, error_message, strlen(error_message));
+	// write(STDERR_FILENO, error_message, strlen(error_message));
 	// execv... call the system exit() method
+	exit(0);
 }
+
+void whoosh_cd()
+{
+
+}
+
+// also have to implement
+// pwd and path... the rest, just check
+// if the command is a built-in command
+
+// ^ jk... if it's... exit, cd, pwd, or path...
+// then don't create a new process. Just use the built in.
+// Else, create a new process to run the command/program
 
 
